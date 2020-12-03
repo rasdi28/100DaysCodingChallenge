5c74ad410833f58aa972262e8bde0d12b9666d76
soniamarcel <75406192+soniamarcel@users.noreply.github.com>
2020-12-03 21:16:09 +0700

    Create Hello Word.cpp

---
+++ Hello Word.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+main()
+{
+  cout<<endl;
+  cout<<"    Hello World     "<<endl;
+  cout<<---------------------"<<endl<<endl;
+  cout<<" by Sonia Marselina "<<endl;
+}
