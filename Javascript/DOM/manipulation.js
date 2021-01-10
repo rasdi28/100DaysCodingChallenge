099803af98016070dd995d9789448f986f66f46a
rasdi28 <rasdi.abdulrohman15120@student.unsika.ac.id>
2021-01-10 22:26:18 +0700

    DOM

---
+++ Javascript/DOM/manipulation.js
@@ -0,0 +1,22 @@
+// const judul = document.getElementById ('judul');
+
+// judul.innerHTML = '<em>Rasdi Abdulrohman</em>';
+
+// const sectionA = document.querySelector('section#a');
+// sectionA.innerHTML = '<div><p>Paragraf1 </p></div>';
+
+
+//element.style.<proportiesSCC>
+
+
+// const judul = document.querySelector('#judul');
+// judul.style.color = 'lightblue';
+// judul.style.backgroundColor = 'salmon';
+
+
+const judul = document.getElementsByTagName('h1')[0];
+
+judul.setAttribute('name','rasdi a');
+
+const a = document.querySelector('section#a a');
+
