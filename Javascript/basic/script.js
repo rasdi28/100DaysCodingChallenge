e94363ee4421e229a1e9b07ceb39f7ed9e89f513
rasdi28 <rasdi.abdulrohman15120@student.unsika.ac.id>
2021-01-14 23:15:52 +0700

    function

---
+++ Javascript/basic/script.js
@@ -0,0 +1,13 @@
+
+// function showMessage(){
+//     alert ('Hello everyone!');
+// }
+
+// local variable
+
+function showMessage(){
+    let message = "Hello, I'm Javascript"; //local variabel
+    alert(message);
+}
+showMessage(); //Hello, I am Javascript
+alert(message);
